ced1919fdc3cac5532561d1557b42e694bfacd20
Edmond Leung <leung.edmond@gmail.com>
2012-07-20 18:06:45 -0400

    Remove company name from top comment

--- Backbone/BackboneCollection.m
+++ Backbone/BackboneCollection.m
@@ -3,7 +3,7 @@
 //  Backbone
 //
 //  Created by Edmond Leung on 6/28/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012. All rights reserved.
 //
 
 #import "BackboneCollection.h"
